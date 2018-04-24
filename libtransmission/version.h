7e9fc56058122355c34e11fe08d82e755c9a92f8
leon <leon.li@ff.com>
2018-04-24 16:51:04 -0700

    Init commit

---
+++ libtransmission/version.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define PEERID_PREFIX             "-TR2930-"
+#define USERAGENT_PREFIX          "2.93"
+#define VCS_REVISION              "3c5870d4f5"
+#define VCS_REVISION_NUM          3c5870d4f5
+#define SHORT_VERSION_STRING      "2.93"
+#define LONG_VERSION_STRING       "2.93 (3c5870d4f5)"
+#define VERSION_STRING_INFOPLIST  2.93
+#define MAJOR_VERSION             2
+#define MINOR_VERSION             93
+#define TR_STABLE_RELEASE         1
